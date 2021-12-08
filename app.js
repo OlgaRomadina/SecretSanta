b7957daef863894809cf2ec7a1598ee9c20f0536
Olga <anugallaguna@gmail.com>
2021-12-08 20:13:23 +0300

    router /login

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const morgan = require('morgan');
 const session = require('express-session');
 const Filestore = require('session-file-store')(session);
 const path = require('path');
-const lkRouter = require('./routes/lk.routes');
 const bcrypt = require('bcrypt');
-const {User} = require('./db/models/');
+const lkRouter = require('./routes/lk.routes');
+const loginRouter = require('./routes/login');
+
+const { User } = require('./db/models');
 
 const PORT = process.env.PORT ?? 3000;
 
@@ -13,56 +15,26 @@ const app = express();
 
 const sessionConfig = {
   store: new Filestore(),
-  name: "user_sid",
-  secret: process.env.SECRET ?? "BIG_SECRET",
+  name: 'user_sid',
+  secret: process.env.SECRET ?? 'BIG_SECRET',
   resave: false,
-  saveUnitialized: false,
+  saveUninitialized: false,
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7,
     httpOnly: true,
   },
 };
 
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join((__dirname, "public"))));
+app.use(express.static(path.join((__dirname, 'public'))));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(session(sessionConfig));
 
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({
-    where: {
-      email,
-    },
-  });
-  if (!user) {
-    res.json({ isUser: false });
-    return;
-  }
-
-  const isAuthenticate = await bcrypt.compare(password, user.password);
-
-  if (isAuthenticate) {
-    req.session.name = user.name;
-    req.json({
-      isCorrectPassword: true,
-      name: user.name,
-    });
-    return;
-  }
-
-  res.json({ isCorrectPassword: false });
-});
-
-app.post("/registration", async (req, res) => {
+app.post('/registration', async (req, res) => {
   const findLogin = await User.findOne({
     where: {
       login: req.body.login,
@@ -70,31 +42,33 @@ app.post("/registration", async (req, res) => {
   });
 
   if (findLogin) {
-    res.render("alreadyRegistered");
+    res.render('alreadyRegistered');
   } else {
-    if(req.body.password1 === req.body.password2){
-    const hashedPassword = await bcrypt.hash(req.body.password1, 8);
-    await User.create({
-      login: req.body.login,
-      email: req.body.email,
-      isAdmin: false,
-      password: hashedPassword,
-    });
-  
-    res.render("lk");
-  } else {
-      res.json({error: 'no'})
+    // eslint-disable-next-line no-lonely-if
+    if (req.body.password1 === req.body.password2) {
+      const hashedPassword = await bcrypt.hash(req.body.password1, 8);
+      await User.create({
+        login: req.body.login,
+        email: req.body.email,
+        isAdmin: false,
+        password: hashedPassword,
+      });
+
+      res.render('lk');
+    } else {
+      res.json({ error: 'no' });
     }
   }
 });
 
-app.get("/registration", async (req, res) => {
-  res.render("registration");
+app.get('/registration', async (req, res) => {
+  res.render('registration');
 });
 
 app.use('/lk', lkRouter);
+app.use('/login', loginRouter);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log("Сервер слушает порт ", PORT);
+  console.log('Сервер слушает порт ', PORT);
 });
