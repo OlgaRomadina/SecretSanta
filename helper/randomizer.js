1b03101beb2ffdb550a87ebe25b091be37e7ea23
Olga <anugallaguna@gmail.com>
2021-12-09 16:33:36 +0300

    randomizer0/0/0

--- helper/randomizer.js
+++ helper/randomizer.js
@@ -3,5 +3,5 @@ const { Card } = require('../db/models');
 const cards = await Card.FindAll();
 cards.map((card, index, arrayCards) => {
   const randomCard = Math.round(-0.5 + Math.random(arrayCards.length + 1) * (1));
-  if(arrayCards[randomCard] !== )
+  if(arrayCards[randomCard].user_id !== card.user_id && arrayCards[randomCard].giver_id !==  )
 });
