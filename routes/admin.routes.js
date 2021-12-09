f2d44750afdb7dfb33bfe828b6c49ab31a0d35db
Maxi2695 <Maxi2695@yandex.ru>
2021-12-09 17:16:47 +0300

    admin updatre 1

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,12 +2,13 @@ const router = require('express').Router();
 const { Card } = require('../db/models');
 
 router.get('/', async (req, res) => {
-const cards = await Card.findAll()
-
-
-
-
-  res.render('adminLk')
+const cards = await Card.findAll({
+  raw: true
+})
+console.log(cards);
+  res.render('adminLk', {
+    cards: cards
+  })
 }) 
 
 module.exports = router;
