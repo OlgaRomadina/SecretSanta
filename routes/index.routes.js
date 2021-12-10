674a6b99fb6d6ad86503ce124f3788821908b025
BarkhatovAndrew <barkhatovandrew@gmail.com>
2021-12-10 12:24:10 +0300

    fix main menu

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { isUser: req.session });
 });
 
 module.exports = router;
